c394bfb39ee55675c1b462c9633e236dcff9e387
Sultan Aziz <hug08153@adobe.com>
2019-03-03 23:51:44 +0530

    Updates

---
+++ precache-manifest.3fa45c4cf327b3516c5063124924e217.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sultan/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c7f1aae7260c8a4b1e55",
+    "url": "/sultan/static/js/runtime~main.c7f1aae7.js"
+  },
+  {
+    "revision": "90aa09ee150325c02556",
+    "url": "/sultan/static/js/main.90aa09ee.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/sultan/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "90aa09ee150325c02556",
+    "url": "/sultan/static/css/main.3f0319c6.chunk.css"
+  },
+  {
+    "revision": "15ae02f139038fa92cf57648fd136573",
+    "url": "/sultan/index.html"
+  }
+];
